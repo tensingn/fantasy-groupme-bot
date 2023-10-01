75069569cb725e26e0cb1389c5d04f92734f331a
tensingn <tensingn@oregonstate.edu>
2023-10-01 17:31:01 -0400

    updating to run with cf

--- index.ts
+++ index.ts
@@ -1,5 +1,13 @@
+import { CloudEvent, Context } from "@google-cloud/functions-framework";
 import { Bot } from "./bot/bot.service";
 
-const dickBotkus = new Bot();
-dickBotkus.runWeek();
-//dickBotkus.runUpdateRosters();
+/**
+ * Triggered from a message on a Cloud Pub/Sub topic.
+ *
+ * @param {!Object} event Event payload.
+ * @param {!Object} context Metadata for the event.
+ */
+exports.runBot = (event: CloudEvent<string>, context: Context) => {
+	const dickBotkus = new Bot();
+	dickBotkus.runWeek();
+};
