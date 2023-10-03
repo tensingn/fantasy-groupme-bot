5866db7b8c34585a112d9d8bcef21b5fddfceba0
tensingn <tensingn@oregonstate.edu>
2023-10-03 18:18:01 -0400

    adding gpt service and secrets service and allowing different teams to be passed in the function

---
+++ gcp/secret.service.ts
@@ -0,0 +1,25 @@
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
+
+export class SecretService {
+	private readonly client: SecretManagerServiceClient;
+
+	constructor() {
+		this.client = new SecretManagerServiceClient({
+			projectId: "nicks-fun-random-projects",
+		});
+	}
+
+	async getSecret(secretName: string): Promise<string> {
+		const secretStuff = await this.client.accessSecretVersion({
+			name: `projects/nicks-fun-random-projects/secrets/${secretName}/versions/latest`,
+		});
+
+		const secret = secretStuff[0].payload?.data?.toString();
+
+		if (!secret) {
+			throw new Error("couldn't get secret");
+		}
+
+		return secret;
+	}
+}
--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ import { Bot } from "./bot/bot.service";
  * @param {!Object} event Event payload.
  * @param {!Object} context Metadata for the event.
  */
-exports.runBot = (event: CloudEvent<string>, context: Context) => {
+exports.runBot = async (event: CloudEvent<string>, context: Context) => {
 	const dickBotkus = new Bot();
-	dickBotkus.runWeek();
+	dickBotkus.runWeek(event.data ?? "");
 };
